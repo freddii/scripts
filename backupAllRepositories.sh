62f2b099a96378d4a8e56a21c47b2d077d7c1b09
freddii <https://freddii@github.com>
2019-05-16 22:26:24 +0200

    added backup all git repos

---
+++ backupAllRepositories.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# example usage:
+#./backupAllRepositories.sh geek1011
+FOLDER=$(date +%B-%d-%Y_%I-%M-%p)
+rm -rf $FOLDER
+mkdir $FOLDER
+cd $FOLDER
+
+USER=$1;
+PAGE=1; 
+curl "https://api.github.com/users/$USER/repos?page=$PAGE&per_page=100" | grep -e 'git_url*' | cut -d \" -f 4 | xargs -L1 git clone
