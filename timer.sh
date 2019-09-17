56d775895159b5c9dcb8c78dd19425de9c8f4411
freddii <https://freddii@github.com>
2019-09-17 11:13:38 +0200

    update

--- timer.sh
+++ timer.sh
@@ -3,6 +3,6 @@
 REMINDER=`zenity --entry --title="Timer" \
  --text="What should I do?"`
 WAIT=`zenity --entry --title="Timer" \
- --text="When should I do this?"`
+ --text="In how many seconds should i do this?"`
 sleep $WAIT
 zenity --info --title="Timer" --text="$REMINDER"
