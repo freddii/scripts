c711027ad9bf76cab4859f53f57f65fd665371a8
freddii <https://freddii@github.com>
2020-04-22 18:35:34 +0200

    update

--- webradio.sh
+++ webradio.sh
@@ -2,10 +2,7 @@
 declare -A SENDER
 SENDER["WDR2"]="http://wdr-wdr2-rheinland.icecast.wdr.de/wdr/wdr2/rheinland/mp3/128/stream.mp3"
 SENDER["WDR5"]="http://wdr-wdr5-live.icecast.wdr.de/wdr/wdr5/live/mp3/128/stream.mp3"
-SENDER["Hellweg Radio"]="http://87.118.64.215:8000/"
 SENDER["Rock Antenne"]="http://mp3.webradio.rockantenne.de:80"
-SENDER["Bayern 3"]="http://gffstream.ic.llnwd.net/stream/gffstream_w12b"
-SENDER["Radio Berlin"]="http://rbb.ic.llnwd.net/stream/rbb_radioberlin_mp3_m_b"
 SENDER["Musicalradio"]="http://mp3.musicalradio.de/musicalradio.mp3"
 SENDER["Radio Swiss Jazz"]="http://stream.srg-ssr.ch/rsj/mp3_128.m3u"
 SENDER["lautFM JazzLoft"]="http://stream.laut.fm/jazzloft"
