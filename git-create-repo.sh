9c157e2e79e960dbcd6769acf19d4ebfb09ffd8e
freddii <https://freddii@github.com>
2020-04-24 09:32:32 +0200

    update

--- git-create-repo.sh
+++ git-create-repo.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 #Usage:
 #git-create-repo.sh GithubUsername NewReponame
-user=$1
-repo_name=$2
+github_username=""
+github_token=""
+
+repo_name=$1
+
+if [ -z "$github_username" ]
+then
+      echo "variable \$github_username is empty, please set variable in this script first."
+      exit 1
+#else
+#      echo "\$github_username is NOT empty"
+fi
+
+if [ -z "$github_token" ]
+then
+      echo "variable \$github_token is empty, please set variable in this script first."
+      exit 1
+#else
+#      echo "\$github_token is NOT empty"
+fi
 
 git init
 if [ ! -f "$PWD/README.md" ]; then
@@ -10,9 +28,23 @@ echo "# $repo_name" > README.md
 fi
 git add .
 git commit -m "First commit"
-
-param="{\"name\":\"$repo_name\"}"
-curl -u $user https://api.github.com/user/repos -d $param
-$pw
-git remote add origin "https://github.com/$user/$repo_name.git"
+#
+#param="{\"name\":\"$repo_name\"}"
+#curl -u $user https://api.github.com/user/repos -d $param
+#$pw
+#
+curl -i -H "Authorization: token $git_token" \
+    -d '{ \
+        "name": $repo_name, \
+        "auto_init": true, \
+        "private": true, \
+        "gitignore_template": "nanoc" \
+      }' \
+    https://api.github.com/user/repos
+#
+git remote add origin "https://github.com/$github_username/$repo_name.git"
 git push origin master
+
+
+
+
