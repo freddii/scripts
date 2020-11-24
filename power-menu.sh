775e747a481bd773a01ee810311526fba9f8aba6
freddii <https://freddii@github.com>
2020-11-24 14:20:14 +0100

    update

---
+++ power-menu.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+usage() {
+    printf "Usage: %s [OPTION]...
+    Select and perform actions about power management
+
+    -h          print this help and exit\n" "$program"
+}
+
+die() {
+    printf "%s\n" "$@"
+    exit 1
+} >&2
+
+program="${0##*"/"}"
+actions=( "Logout" "Poweroff" "Reboot" "Suspend" )
+
+while getopts ":h" opt; do
+    case "$opt" in
+        "h" )   usage && exit                           ;;
+        * )     die "$program: $OPTARG: invalid option" ;;
+    esac
+done
+
+# wofi [-d|--dmenu] [-L|--lines]
+selection="$(printf "%s\n" "${actions[@]}" | wofi -dL 4)"
+
+case "$selection" in
+    "Logout" )      loginctl terminate-user "$USER" ;;
+    "Poweroff" )    systemctl poweroff              ;;
+    "Reboot" )      systemctl reboot                ;;
+    "Suspend" )     systemctl suspend               ;;
+esac
